31e972c2a2f8079e93f9b9e1adf7d335bf7da18f
Corin Chaplin <git@corinchaplin.co.uk>
2015-11-12 17:45:46 +0000

    Update GUI for login so that a signup button is also available

--- static/assets/js/index-scripts.js
+++ static/assets/js/index-scripts.js
@@ -1,41 +1,47 @@
 $(function() {
-  $('#signup-btn').on('click', function () {
+  $(document).on('click', '.signup-btn', function () {
     loadXMLDoc("signup");
   });
 
-  $('#login-btn').on('click', function () {
+  $(document).on('click', '.login-btn', function () {
     loadXMLDoc("login");
   });
-});
 
-function loadXMLDoc(page) {
-  var xmlHttp;
 
-  if (window.XMLHttpRequest) {
-    xmlHttp = new XMLHttpRequest();
-  } else {
-    xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
 
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == XMLHttpRequest.DONE ) {
-      if(xmlHttp.status == 200){
-        document.getElementById("main-content-area").innerHTML = xmlHttp.responseText;
-      }
-      else if(xmlHttp.status == 400) {
-        alert('There was an error 400')
-      }
-      else {
-        alert('something else other than 200 was returned')
+  
+  function loadXMLDoc(page) {
+    var xmlHttp;
+
+    if (window.XMLHttpRequest) {
+      xmlHttp = new XMLHttpRequest();
+    } else {
+      xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == XMLHttpRequest.DONE ) {
+        if(xmlHttp.status == 200){
+          document.getElementById("main-content-area").innerHTML = xmlHttp.responseText;
+        }
+        else if(xmlHttp.status == 400) {
+          alert('There was an error 400')
+        }
+        else {
+          alert('something else other than 200 was returned')
+        }
       }
     }
+
+    if (page === "signup") {
+      xmlHttp.open("GET", "/views/signup.html", true);
+    } else if (page === "login") {
+      xmlHttp.open("GET", "/views/login.html", true);
+    }
+
+    xmlHttp.send();
   }
 
-  if (page === "signup") {
-    xmlHttp.open("GET", "/views/signup.html", true);
-  } else if (page === "login") {
-    xmlHttp.open("GET", "/views/login.html", true);
-  }
 
-  xmlHttp.send();
-}
+});
+
