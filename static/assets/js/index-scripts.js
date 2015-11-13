c661212666fefc6df951a7b5656a51f4610ffce1
Corin Chaplin <git@corinchaplin.co.uk>
2015-11-13 15:16:14 +0000

    Add some more client side validation and client side sign up requests

--- static/assets/js/index-scripts.js
+++ static/assets/js/index-scripts.js
@@ -46,6 +46,49 @@ $(function() {
     });
   });
 
+  $(document).on('keyup', '#signup_password', function (event){
+    var v = $(event.currentTarget).val();
+    if(v.length >= 6){
+        $(".signup_password-error").addClass("hidden");
+        $(".signup_password-ok").removeClass("hidden");
+      }else{
+        $(".signup_password-ok").addClass("hidden");
+        $(".signup_password-error").removeClass("hidden");
+      }
+  });
+
+  $(document).on('keyup', '#signup_repeatPassword', function (event){
+    var v = $(event.currentTarget).val();
+    if(v === $("#signup_password").val()){
+        $(".signup_repeatPassword-error").addClass("hidden");
+        $(".signup_repeatPassword-ok").removeClass("hidden");
+      }else{
+        $(".signup_repeatPassword-ok").addClass("hidden");
+        $(".signup_repeatPassword-error").removeClass("hidden");
+      }
+  });
+
+  $(document).on('change', '#signup_dob', function (event){
+    var v = new Date($(event.currentTarget).val());
+    if(v < new Date(Date.now() - (1000 * 60 * 60 * 24 * 365 * 10))){
+        $(".signup_dob-error").addClass("hidden");
+        $(".signup_dob-ok").removeClass("hidden");
+      }else{
+        $(".signup_dob-ok").addClass("hidden");
+        $(".signup_dob-error").removeClass("hidden");
+      }
+  });
+
+  $(document).on('click', '.signup-submit', function (){
+    var username = $("#signup_username").val(),
+      password = $("#signup_password").val(),
+      dob = $("#signup_dob").val();
+
+    comms.user_management.sign_up(username, password, dob, function(data){
+      console.log(data);
+    });
+  });
+
 
 });
 
