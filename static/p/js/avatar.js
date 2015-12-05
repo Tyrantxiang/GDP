46498db3e30a5666061bbe83ad77d174a95f0c52
Trisha Khallaghi <tk7g11@soton.ac.uk>
2015-12-05 15:46:05 +0000

    fixed avatar screen bug

--- static/p/js/avatar.js
+++ static/p/js/avatar.js
@@ -29,97 +29,108 @@
       canvas.height / 2 - img.width / 2);
   }
 
-window.hub.avatarCreationLoader = function(){
-  $.get("/views/createavatar.html", function(data){
-    $("#avatar-creation-overlay").html(data);
-
-
-    
-    // Draw white background on canvas
-    var canvas = document.getElementById("avatar-create");
+  window.hub.avatarCreationLoader = function(){
+    $.get("/views/createavatar.html", function(data){
+      $("#avatar-creation-overlay").html(data);
+      
+      // Draw white background on canvas
+      var canvas = document.getElementById("avatar-create");
       ctx = canvas.getContext("2d");
-    ctx.roundRect(0, 0, canvas.width, canvas.height, canvasRadius);
-    ctx.fillStyle = "#ffffff";
-    ctx.fill();
+      ctx.roundRect(0, 0, canvas.width, canvas.height, canvasRadius);
+      ctx.fillStyle = "#ffffff";
+      ctx.fill();
 
-    // Cached image object
-    var img;
+      // Cached image object
+      var img;
+
+      // Cache the menus divs
+      var menus = {
+        head : $('#head-menu'),
+        eyes : $('#eyes-menu'),
+        shirt : $('#upper-menu'),
+        skin : $('#skin-menu'),
+        trousers : $('#lower-menu')
+      };
 
 
-    // Cache the menus divs
-    var menus = {
-      head : $('#head-menu'),
-      eyes : $('#eyes-menu'),
-      shirt : $('#upper-menu'),
-      skin : $('#skin-menu'),
-      trousers : $('#lower-menu')
-    };
-
-
-    hub.getUserUnlockedItems(function (items) {
-      var latch = (function(num, complete){
-        return function(){
-          num--;
-          if(num === 0){
-              complete();
+      hub.getUserUnlockedItems(function (items) {
+        var latch = (function(num, complete){
+          return function(){
+            num--;
+            if(num === 0){
+                complete();
+            }
           }
-        }
-      })(Object.keys(items).length, function(){
-        menus.head.find('img:first').addClass('active');
-        menus.eyes.find('img:first').addClass('active');
-        menus.shirt.find('img:first').addClass('active');
-        menus.skin.find('img:first').addClass('active');
+        })(Object.keys(items).length, function(){
+          window.comms.get_user_equipped_items(function(data) {
+            var curHeadId = data.head.id;
+            var curEyesId = data.eyes.id;
+            var curShirtId = data.shirt.id;
+            var curSkinId = data.skin.id;
 
+            menus.head.find('img').map(function() {
+              setEquippedItemAsActive(data.head.id, this);
+            });
+
+            menus.eyes.find('img').map(function() {
+              setEquippedItemAsActive(data.eyes.id, this);
+            });
+
+            menus.shirt.find('img').map(function() {
+              setEquippedItemAsActive(data.shirt.id, this);
+            });
+
+            menus.skin.find('img').map(function() {
+              setEquippedItemAsActive(data.skin.id, this);
+            });
+
+            function setEquippedItemAsActive(itemId, item) {
+              if (itemId == $(item).data("itemId")) {
+                $(item).addClass('active');
+              }
+            }
+
+            equipItemsOnSelect();
+          });
+        });
+
+        for (var i in items) {       
+          hub.getItemInfo(items[i], function (item) {    
+            var imgEl = $(document.createElement("img")).attr("src", item.url).data("itemId", item.id).addClass("col-md-3 white-img-box"),
+              menu = menus[item.slot];
+
+              if(menu){
+                menu.append(imgEl);
+              }
+
+              latch();
+          });
+        }
+      });
+      
+      $(document).on("click", '.white-img-box', function(e) {
+        $('.white-img-box', e.target.parentNode).removeClass('active');
+        $(this).addClass('active');
         equipItemsOnSelect();
       });
 
-      for (var i in items) {       
-        hub.getItemInfo(items[i], function (item) {    
-          var imgEl = $(document.createElement("img")).attr("src", item.url).data("itemId", item.id).addClass("col-md-3 white-img-box"),
-            menu = menus[item.slot];
 
-            if(menu){
-              menu.append(imgEl);
-            }
+      $("#avatar-creation-close").click(function(){
+        hub.closeAvatarCreation();
+      });
 
-            latch();
+      function equipItemsOnSelect() {
+        var userObj = {
+          head : menus.head.find('.active').data("itemId"),
+          eyes : menus.eyes.find('.active').data("itemId"),
+          skin : menus.skin.find('.active').data("itemId"),
+          shirt : menus.shirt.find('.active').data("itemId")
+        };
+        hub.updateEquiptItems(userObj, function(image) {
+          img = image;
+          drawAvatar(canvas, image, "#ffffff");
         });
       }
     });
-    
-    $(document).on("click", '.white-img-box', function(e) {
-      $('.white-img-box', e.target.parentNode).removeClass('active');
-      $(this).addClass('active');
-      equipItemsOnSelect();
-    });
-
-
-    $("#avatar-creation-close").click(function(){
-      hub.closeAvatarCreation();
-    });
-
-    /* No server side implementation 
-    $('#favcolor').on("input", function(e) {
-      equipItemsOnSelect();
-    });*/
-
-
-    function equipItemsOnSelect() {
-      //var bgColor = $('#favcolor').val() ? $('#favcolor').val() : "#ffffff";
-      var userObj = {
-        head : menus.head.find('.active').data("itemId"),
-        eyes : menus.eyes.find('.active').data("itemId"),
-        skin : menus.skin.find('.active').data("itemId"),
-        shirt : menus.shirt.find('.active').data("itemId")
-      };
-      hub.updateEquiptItems(userObj, function(image) {
-        img = image;
-        drawAvatar(canvas, image, "#ffffff");
-      });
-      
-    }
-
-  });
-}
-
+  } 
 })();
