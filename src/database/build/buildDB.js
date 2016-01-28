33f3b976d36153da999323845a72bb9778610c7c
Corin Chaplin <git@corinchaplin.co.uk>
2016-01-28 16:15:41 +0000

    Fix buildDB.js to run on windows

--- src/database/build/buildDB.js
+++ src/database/build/buildDB.js
@@ -9,53 +9,66 @@
 */
 
 
-function createBuildScript(){
-	fs.readFile(__dirname+'/schema.sql', function(err, data){
-		if(err) { return fail(err); }
+var isWindows = process.platform === "win32",
+	fs = require("fs"),
+	path = require("path"),
 
-		var buildScript = data.toString().replace(/{schema}/g, settings.schema);
+	config = require('../../config.js'),
+	db = require('../database.js');
 
-		fs.writeFile(__dirname+'/build.sql', buildScript, function(err, data){
-			if(err) { return fail(err) };
 
-			runBuildScript();
-		});
-
-	});
-}
-
-function runBuildScript(){
-	var command = [
-		"export PGPASSWORD="+settings.password
-		, "psql -q -h "+settings.hostname+" -U "+settings.username+" -d "+settings.database+" -c '\\i "+__dirname+"/build.sql'"
-		, "export PGPASSWORD=foo" //resets password to something unrecognisable
-	].join("; ");
-
-	var exec = require('child_process').exec;
-	exec(command, function(error, stdout, stderr){
-		if (error !== null) {
-			fail(error);
-		} else if(stderr.indexOf("FATAL") > -1){
-			fail(stderr);
-		} else {
-			pass({
-				stdout: stdout
-				, stderr: stderr
-			});
-		}
-	});
-}
 
 module.exports = function build(pass, fail, databaseName){
+	var settings;
+
+
+
+	function createBuildScript(){
+		fs.readFile(path.join(__dirname, '/schema.sql'), function(err, data){
+			if(err) { return fail(err); }
+
+			var buildScript = data.toString().replace(/{schema}/g, settings.schema);
+
+			fs.writeFile(path.join(__dirname, '/build.sql'), buildScript, function(err, data){
+				if(err) { return fail(err) };
+
+				runBuildScript();
+			});
+
+		});
+	}
+
+	function runBuildScript(){
+		var exp = (isWindows) ? "set" : "export";
+
+		var command = [
+			  exp + " PGPASSWORD="+settings.password
+			, 'psql -q -h ' + settings.hostname + ' -U ' + settings.username + ' -d ' + settings.database + ' -c "\\i ' + path.join(__dirname, '/build.sql') + '"'
+			, exp + " PGPASSWORD=foo" //resets password to something unrecognisable
+		].join("; ");
+
+		var exec = require('child_process').exec;
+		exec(command, function(error, stdout, stderr){
+			if (error !== null) {
+				fail(error);
+			} else if(stderr.indexOf("FATAL") > -1){
+				fail(stderr);
+			} else {
+				pass({
+					stdout: stdout
+					, stderr: stderr
+				});
+			}
+		});
+	}
+
+
+
 
 	if(!databaseName){
 		return fail("******* YOU NEED TO PROVIDE DATABASE NAME ***********");
 	} else {
-		var fs = require("fs")
-				, config = require('../../config.js')
-				, db = require('../database.js');
-
-		var settings = config.database.getSettings(databaseName);
+		settings = config.database.getSettings(databaseName);
 
 		db.init(createBuildScript, fail, settings);
 
