391f12b0682f5790bdce19cdb4020e60d61b3e29
Corin Chaplin <git@corinchaplin.co.uk>
2016-01-28 15:45:39 +0000

    Fix buildBD errors, add new buildBD command to package.json

--- src/database/build/buildDB.js
+++ src/database/build/buildDB.js
@@ -8,7 +8,46 @@
  * @authors Joe Ringham
 */
 
+
+function createBuildScript(){
+	fs.readFile(__dirname+'/schema.sql', function(err, data){
+		if(err) { return fail(err); }
+
+		var buildScript = data.toString().replace(/{schema}/g, settings.schema);
+
+		fs.writeFile(__dirname+'/build.sql', buildScript, function(err, data){
+			if(err) { return fail(err) };
+
+			runBuildScript();
+		});
+
+	});
+}
+
+function runBuildScript(){
+	var command = [
+		"export PGPASSWORD="+settings.password
+		, "psql -q -h "+settings.hostname+" -U "+settings.username+" -d "+settings.database+" -c '\\i "+__dirname+"/build.sql'"
+		, "export PGPASSWORD=foo" //resets password to something unrecognisable
+	].join("; ");
+
+	var exec = require('child_process').exec;
+	exec(command, function(error, stdout, stderr){
+		if (error !== null) {
+			fail(error);
+		} else if(stderr.indexOf("FATAL") > -1){
+			fail(stderr);
+		} else {
+			pass({
+				stdout: stdout
+				, stderr: stderr
+			});
+		}
+	});
+}
+
 module.exports = function build(pass, fail, databaseName){
+
 	if(!databaseName){
 		return fail("******* YOU NEED TO PROVIDE DATABASE NAME ***********");
 	} else {
@@ -20,42 +59,7 @@ module.exports = function build(pass, fail, databaseName){
 
 		db.init(createBuildScript, fail, settings);
 
-		function createBuildScript(){
-			fs.readFile(__dirname+'/schema.sql', function(err, data){
-				if(err) { return fail(err); }
 
-				var buildScript = data.toString().replace(/{schema}/g, settings.schema);
-				
-				fs.writeFile(__dirname+'/build.sql', buildScript, function(err, data){
-					if(err) { return fail(err) };
-
-					runBuildScript();
-				});
-
-			});
-		}
-
-		function runBuildScript(){
-			var command = [
-				"export PGPASSWORD="+settings.password
-				, "psql -q -h "+settings.hostname+" -U "+settings.username+" -d "+settings.database+" -c '\\i "+__dirname+"/build.sql'"
-				, "export PGPASSWORD=foo" //resets password to something unrecognisable
-			].join("; ");
-
-			var exec = require('child_process').exec;
-			exec(command, function(error, stdout, stderr){
-				if (error !== null) {
-					fail(error);
-				} else if(stderr.indexOf("FATAL") > -1){
-					fail(stderr);
-				} else {
-					pass({
-						stdout: stdout
-						, stderr: stderr
-					});
-				}
-			});
-		}
 	}
 };
 
