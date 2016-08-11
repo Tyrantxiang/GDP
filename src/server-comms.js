23093886a428db6c2423e3ad6f7048f9ab6da684
clouds8 <clouds8@sina.cn>
2016-08-11 23:05:27 +0800

    move the connect_middleware and auth.socket_middleware to io.on('connection')

--- src/server-comms.js
+++ src/server-comms.js
@@ -67,10 +67,14 @@ Comms.prototype.send = {
  * @param {module:auth~auth}       auth   - The authentication object
  * @param {module:config}          config - The config.js object
  * @param {module:hub~hub}         hub    - The hub object
- * @return {external:"socket.io"~socket} The initialised socket.io socket object 
+ * @return {external:"socket.io"~socket} The initialised socket.io socket object
  */
 module.exports = function (server, auth, config, hub){
     var io = require("socket.io")(server);
+
+    // record players online     key:value  ==>  socket.userId: socket
+    // By clouds8
+    var sockets = {};
 
     function connect_middleware(socket, next){
         console.log("      uid " + socket.userId + ": socket created");
@@ -97,14 +101,59 @@ module.exports = function (server, auth, config, hub){
 
 
     // Set the auth middleware
-    io.use(auth.socket_middleware);
+    // io.use(auth.socket_middleware);
 
     // Setup middleware (bit of a hack)
-    io.use(connect_middleware);
+    // io.use(connect_middleware);
 
-    /*io.on("connection", function (socket){
 
-    });*/
+    /**
+     * move the connect_middleware and auth.socket_middleware to io.on("connection")
+     *By clouds8
+     *
+     * @param  {[type]} "connection" [description]
+     * @param  {[type]} deal with connection
+     * @return {[type]}              [description]
+     */
+    io.on("connection", function (socket){
+      var auth_socket_middleware = new Promise(function(resolve, reject) {
+          auth.socket_middleware(socket, function (err) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          })
+        });
+
+        auth_socket_middleware
+        .then(function () {
+          if(socket.userId) {
+              sockets[socket.userId] = socket;
+              // console.log('show the sockets alive:')
+              // console.log(sockets);
+          }
+
+          socket.on("disconnect", function(){
+              console.log("user " + socket.userId + "  left");
+              delete sockets[socket.userId];
+          });
+        })
+        .then(function () {
+          connect_middleware(socket, function (err) {
+            if (err) {
+              throw err;
+            } else {
+                return;
+            }
+          });
+        })
+        .catch(
+          function (err) {
+            console.error(err);
+          }
+        )
+    });
 
 
     return io;
