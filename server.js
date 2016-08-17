4c43ef29d2f0115ce0b42c8e88194cfd3fc2c3b7
Tyrantxiang <127272006@qq.com>
2016-08-17 07:47:53 +0100

    Add files via upload
    聊天室的代码

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var users=[];
+app.use('/', express.static(__dirname + '/www'));
+
+server.listen(8080);
+
+ io.on('connection',function(socket){
+	socket.on('login',function(nickname){
+		if(users.indexOf(nickname) > -1){
+			socket.emit('nickExisted');	
+		}else{
+			socket.userIndex = users.length;
+			socket.nickname = nickname;
+			users.push(nickname);
+			socket.emit('loginSuccess');
+			io.sockets.emit('system',nickname,users.length,'login');
+		};
+	});
+	socket.on('disconnect',function(){
+		users.splice(socket.userIndex,1);
+		socket.broadcast.emit('system',socket.nickname,users.length,'logout');
+	});
+	socket.on('postMsg', function(msg){
+		socket.broadcast.emit('newMsg', socket.nickname, msg);
+	});
+	socket.on('img',function(imgData){
+		socket.broadcast.emit('newImg',socket.nickname,imgData);
+	});
+});
